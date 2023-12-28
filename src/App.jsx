167ad8eff3fae4d67e57ab759e5589211dacd18f
Sola Dugbo <sola.dugbo@publicissapient.com>
2023-12-28 01:50:47 -0500

    changed content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
-import { useState } from "react";
+import React from "react";
+import RepoStats from "./components/RepoStats.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Linkbar from "./components/Linkbar.jsx";
+import Intro from "./components/Intro.jsx";
+import About from "./components/About.jsx";
+import Education from "./components/Education.jsx";
+import WorkExperience from "./components/WorkExperience.jsx";
+import SideProjects from "./components/SideProjects.jsx";
+import Footer from "./components/Footer.jsx";
+import Error404 from "./components/Error404.jsx";
 
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import "./styles/styles.css";
+import content from "./content.json";
+
+// Note that the section.enable_section has to equal true in
+// order for that specific component to load in the app.js
 
 function App() {
-  return <></>;
+  const { intro_screen, about_me, academics, experience, projects } = content;
+
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <Navbar />
+              <RepoStats />
+              <Linkbar />
+              {intro_screen.section.enable_section && <Intro />}
+              {about_me.section.enable_section && <About />}
+              {academics.section.enable_section && <Education />}
+              {experience.section.enable_section && <WorkExperience />}
+              {projects.section.enable_section && <SideProjects />}
+              <Footer />
+            </div>
+          }
+        ></Route>
+        <Route path="*" element={<Error404 />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Education.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import contentData from "../content.json";
+import { Fade } from "react-awesome-reveal";
+
+const Education = () => {
+  // Extract the education data from the contentData file
+  const education = contentData.academics;
+
+  // Calculate the total credits for each year
+  const totalCreditsPerYear = education.years.map((year) => {
+    return parseFloat(
+      year.courses.reduce((total, course) => {
+        return total + course.credits;
+      }, 0)
+    );
+  });
+
+  // Render the education section
+  return (
+    <Fade triggerOnce={true}>
+      <section id="education">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="title-box">
+                <h3 className="title-a">
+                  {education.section.title} <div className="line-mf"></div>
+                </h3>
+                <p className="subtitle-a">{education.section.description}</p>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="row">
+                {education.years.map((year, index) => (
+                  <div className="col-md-6" key={index}>
+                    <Fade
+                      delay={index * 200}
+                      cascade={false}
+                      triggerOnce={true}
+                    >
+                      <div className="education">
+                        <table>
+                          <thead>
+                            <tr>
+                              <th>
+                                {year.semester} {year.year} Courses
+                              </th>
+                              <th>Credits</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {year.courses.map((course, index) => (
+                              <tr key={index}>
+                                <td data-tooltip={course.abbreviation}>
+                                  {course.name}
+                                </td>
+                                <td>{course.credits.toFixed(1)}</td>
+                              </tr>
+                            ))}
+                          </tbody>
+                        </table>
+                      </div>
+                    </Fade>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Fade>
+  );
+};
+
+export default Education;
